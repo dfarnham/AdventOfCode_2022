95648a0ff334701e8cc70182b07b138c09ef81bf
Dave Farnham <dave.farnham@icloud.com>
2022-12-07 18:18:12 -0700

    day_05 part 2

--- day_05/src/main.rs
+++ day_05/src/main.rs
@@ -23,26 +23,48 @@ fn build_stacks(array: &[String]) -> BTreeMap<usize, Vec<char>> {
 fn move_crates(
     array: &[String],
     stacks: &BTreeMap<usize, Vec<char>>,
+    challenge: usize, // 1 or 2
 ) -> Result<String, Box<dyn Error>> {
+    // ** input-example **
+    //     [D]
+    // [N] [C]
+    // [Z] [M] [P]
+    //  1   2   3
+    //
+    // move 1 from 2 to 1
+    // move 3 from 1 to 3
+    // move 2 from 2 to 1
+    // move 1 from 1 to 2
+
+    //
+    // stacks: {0: ['Z', 'N'], 1: ['M', 'C', 'D'], 2: ['P']}
+    //
+    // compute the maximum vector length and skip(n + 2) lines
+    // from the input array to start reading the move instructions
     let n = stacks.values().map(|v| v.len()).max().ok_or("max error")?;
     let mut stacks = stacks.clone();
+
     for line in array.iter().skip(n + 2) {
-        let instructions = line
-            .split_whitespace()
-            .collect::<Vec<_>>();
+        let instructions = line.split_whitespace().collect::<Vec<_>>();
         let count = instructions[1].parse::<usize>()?;
         let source = instructions[3].parse::<usize>()? - 1;
         let destination = instructions[5].parse::<usize>()? - 1;
 
+        let mut tmp = vec![];
         for _ in 0..count {
             if let Some(a) = stacks.get_mut(&source) {
-                let value = a.pop().ok_or("pop() error")?;
-                if let Some(b) = stacks.get_mut(&destination) {
-                    b.push(value);
+                if challenge == 1 {
+                    tmp.push(a.pop().ok_or("pop() error")?);
+                } else {
+                    tmp.insert(0, a.pop().ok_or("pop() error")?);
                 }
             }
         }
+        if let Some(b) = stacks.get_mut(&destination) {
+            b.extend(tmp);
+        }
     }
+
     Ok(stacks
         .values()
         .map(|v| v.last().unwrap().to_string())
@@ -50,15 +72,13 @@ fn move_crates(
         .join(""))
 }
 
-/*
-fn part1(array: &[String]) -> Result<u64, Box<dyn Error>> {
-    Ok(0)
+fn part1(array: &[String], stacks: &BTreeMap<usize, Vec<char>>) -> Result<String, Box<dyn Error>> {
+    move_crates(array, stacks, 1)
 }
 
-fn part2(array: &[String]) -> Result<u64, Box<dyn Error>> {
-    Ok(0)
+fn part2(array: &[String], stacks: &BTreeMap<usize, Vec<char>>) -> Result<String, Box<dyn Error>> {
+    move_crates(array, stacks, 2)
 }
-*/
 
 fn main() -> Result<(), Box<dyn Error>> {
     // behave like a typical unix utility
@@ -74,12 +94,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     // ==============================================================
 
     let stacks = build_stacks(&puzzle_lines);
-    writeln!(
-        stdout,
-        "Answer Part 1 = {}",
-        move_crates(&puzzle_lines, &stacks)?
-    )?;
-    //writeln!(stdout, "Answer Part 2 = {}", part2(&puzzle_lines)?)?;
+    writeln!(stdout, "Answer Part 1 = {}", part1(&puzzle_lines, &stacks)?)?;
+    writeln!(stdout, "Answer Part 2 = {}", part2(&puzzle_lines, &stacks)?)?;
     Ok(())
 }
 
@@ -96,7 +112,7 @@ mod tests {
     fn part1_example() -> Result<(), Box<dyn Error>> {
         let puzzle_lines = get_data("input-example");
         let stacks = build_stacks(&puzzle_lines);
-        assert_eq!(move_crates(&puzzle_lines, &stacks)?, "CMZ");
+        assert_eq!(part1(&puzzle_lines, &stacks)?, "CMZ");
         Ok(())
     }
 
@@ -104,23 +120,23 @@ mod tests {
     fn part1_actual() -> Result<(), Box<dyn Error>> {
         let puzzle_lines = get_data("input-actual");
         let stacks = build_stacks(&puzzle_lines);
-        assert_eq!(move_crates(&puzzle_lines, &stacks)?, "CVCWCRTVQ");
+        assert_eq!(part1(&puzzle_lines, &stacks)?, "CVCWCRTVQ");
         Ok(())
     }
 
-    /*
     #[test]
     fn part2_example() -> Result<(), Box<dyn Error>> {
-        let data = get_data("input-example");
-        assert_eq!(part2(&data)?, 4);
+        let puzzle_lines = get_data("input-example");
+        let stacks = build_stacks(&puzzle_lines);
+        assert_eq!(part2(&puzzle_lines, &stacks)?, "MCD");
         Ok(())
     }
 
     #[test]
     fn part2_actual() -> Result<(), Box<dyn Error>> {
-        let data = get_data("input-actual");
-        assert_eq!(part2(&data)?, 849);
+        let puzzle_lines = get_data("input-actual");
+        let stacks = build_stacks(&puzzle_lines);
+        assert_eq!(part2(&puzzle_lines, &stacks)?, "CNSCZWLVT");
         Ok(())
     }
-    */
 }
